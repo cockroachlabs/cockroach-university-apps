6fc1143ce1ddeee5c0bf8349a091a7b932f0a8de
Felipe Gutierrez <felipe.gutierrezcruz@cockroachlabs.com>
2025-04-13 14:34:12 -0400

    Removing unnecessary imports

--- migration-java-apps/logistics/src/test/java/com/omnicorp/logistics/LogisticsApplicationTests.java
+++ migration-java-apps/logistics/src/test/java/com/omnicorp/logistics/LogisticsApplicationTests.java
@@ -10,11 +10,7 @@ import com.omnicorp.logistics.service.LogisticsService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
