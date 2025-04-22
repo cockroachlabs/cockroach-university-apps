3d62c3ebe66a7d5f5b459f36618f17a09acb72d1
Felipe Gutierrez <felipe.gutierrezcruz@cockroachlabs.com>
2025-04-22 16:32:28 -0400

    updated migration - sql

--- migration-sql/omnicorp_schemas_uuid.sql
+++ migration-sql/omnicorp_schemas_uuid.sql
@@ -1,24 +1,22 @@
 CREATE TABLE customers (
     customer_uuid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    customer_id INT8 NOT NULL,
+    customer_id INT8 NULL,
     first_name VARCHAR(255) NULL,
     last_name VARCHAR(255) NULL,
     email VARCHAR(255) NULL,
     phone VARCHAR(20) NULL,
     address VARCHAR(255) NULL,
-    UNIQUE INDEX customers_email_key (email ASC),
-    UNIQUE INDEX customers_customer_id_key (customer_id ASC) -- Retaining uniqueness on the old ID
+    UNIQUE INDEX customers_email_key (email ASC)
 );
 
 CREATE TABLE orders (
     order_uuid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    order_id INT8 NOT NULL,
+    order_id INT8 NULL,
     customer_id INT8 NULL,
     customer_uuid UUID NULL REFERENCES customers (customer_uuid),
     order_date DATE NULL,
     total_amount DECIMAL(10,2) NULL,
-    CONSTRAINT orders_customer_uuid_fkey FOREIGN KEY (customer_uuid) REFERENCES customers(customer_uuid), 
-    UNIQUE INDEX orders_order_id_key (order_id ASC) -- Retaining uniqueness on the old ID
+    CONSTRAINT orders_customer_uuid_fkey FOREIGN KEY (customer_uuid) REFERENCES customers(customer_uuid)
 );
 
 CREATE TABLE products (
@@ -26,8 +24,7 @@ CREATE TABLE products (
     product_id INT8 NOT NULL,
     product_name VARCHAR(255) NULL,
     description STRING NULL,
-    price DECIMAL(10,2) NULL,
-    UNIQUE INDEX products_product_id_key (product_id ASC) -- Retaining uniqueness on the old ID
+    price DECIMAL(10,2) NULL
 );
 
 CREATE TABLE inventory (
@@ -37,8 +34,7 @@ CREATE TABLE inventory (
     product_uuid UUID NULL REFERENCES products (product_uuid),
     quantity INT8 NULL,
     location VARCHAR(255) NULL,
-    CONSTRAINT inventory_product_uuid_fkey FOREIGN KEY (product_uuid) REFERENCES products(product_uuid), 
-    UNIQUE INDEX inventory_inventory_id_key (inventory_id ASC) -- Retaining uniqueness on the old ID
+    CONSTRAINT inventory_product_uuid_fkey FOREIGN KEY (product_uuid) REFERENCES products(product_uuid)
 );
 
 CREATE TABLE shipments (
@@ -50,6 +46,5 @@ CREATE TABLE shipments (
     product_uuid UUID NULL REFERENCES products (product_uuid),
     quantity INT8 NULL,
     shipment_date DATE NULL,
-    CONSTRAINT shipments_product_uuid_fkey FOREIGN KEY (product_uuid) REFERENCES products(product_uuid), -- Retaining the old foreign key
-    UNIQUE INDEX shipments_shipment_id_key (shipment_id ASC) -- Retaining uniqueness on the old ID
+    CONSTRAINT shipments_product_uuid_fkey FOREIGN KEY (product_uuid) REFERENCES products(product_uuid)
 );
